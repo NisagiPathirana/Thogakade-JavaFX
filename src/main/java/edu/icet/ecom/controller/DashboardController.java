90bb96e0533bbc3525d656b2b04dbea80f4c86d8
NisagiPathirana <kpndomalsa19@gmail.com>
2025-09-13 10:37:31 +0530

    initial commit

---
+++ src/main/java/edu/icet/ecom/controller/DashboardController.java
@@ -0,0 +1,45 @@
+package edu.icet.ecom.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+    public class DashboardController {
+
+        Stage cu
+
+        @FXML
+        private JFXButton btnCustomerMananegement;
+
+        @FXML
+        private JFXButton btnItemMananegement;
+
+        @FXML
+        private JFXButton btnOrderDetailMananegement;
+
+        @FXML
+        private JFXButton btnOrderMananegement;
+
+        @FXML
+        void btnCustomerMananegementOnAction(ActionEvent event) {
+
+
+        }
+
+        @FXML
+        void btnItemMananegementOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void btnOrderDetailMananegementOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void btnOrderMananegementOnAction(ActionEvent event) {
+
+        }
+
+    }
+
